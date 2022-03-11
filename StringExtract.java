a497c716f6d2ab0117255dc3e564b01e5976a2a1
Kritagya Painuly <75200248+KritagyaPainuly@users.noreply.github.com>
2022-03-11 10:42:23 +0530

    Create StringExtract.java

---
+++ StringExtract.java
@@ -0,0 +1,10 @@
+/*Write a program to extractr the portion of a char string and print the extracted string assume that m characters are extracted, starting with the nTH char*/
+import java.*;
+class StringExtract{
+public static void main(String arr[])
+{
+
+}
+} 
+
+	
