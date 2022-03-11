94b98d56da0e8331420a604f413a22fdb6b37ca3
Kritagya Painuly <75200248+KritagyaPainuly@users.noreply.github.com>
2022-03-11 10:41:18 +0530

    Create StringSort.java

---
+++ StringSort.java
@@ -0,0 +1,8 @@
+//Write a program which will read a string and rewrite it in albhabetic order Eg:-STRING GINRST 
+import java.*;
+class StringSort{
+public static void main(String arr[])
+{
+Scanner sc=Sca
+}
+} 
